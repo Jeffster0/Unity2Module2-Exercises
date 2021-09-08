9402eb3defaf67f046ee1717dc608e4e795b2252
Eeeeeeeeeeeeeeeeeeeeeeeeee <69118564+Jeffster0@users.noreply.github.com>
2021-09-08 16:37:34 -0700

    cube controler

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -7,25 +7,28 @@ public class CubeController : MonoBehaviour
 {
     public TMP_Text scoreText;
     private int currentNum;
-    
+    public int score = 0;
+    public Vector3 position;
 
     // Start is called before the first frame update
     void Start()
     {
-        //INITIALIZE CURRENTNUM HERE
+        currentNum = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // currentNum +=1;
+        // print(currentNum);
         //INCREMENT AND PRINT CURRENTNUM HERE 
 
         //Uncomment the code below and complete 
-        //the if statement to check if Mouse0 is clicked 
-        // if ()
-        // {
-        //     score++;
-        //     scoreText.text = "Score: " + score.ToString();
-        // }
+        // the if statement to check if Mouse0 is clicked 
+        if (Input.GetMouseButtonDown(0))
+        {
+            score++;
+            scoreText.text = "Score: " + score.ToString();
+        }
     }
 }
