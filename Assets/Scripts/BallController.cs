10a5bfa8339ada92b6886f1bdba68561ca73f5e2
Eeeeeeeeeeeeeeeeeeeeeeeeee <69118564+Jeffster0@users.noreply.github.com>
2021-09-15 16:37:42 -0700

    yes

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -8,27 +8,28 @@ public class BallController : MonoBehaviour
     public TMP_Text updateText;
     public TMP_Text fixedUpdateText;
     //Declare and intialize updateCounter and fixedUpdateCounter here
-    
+    public float updateCounter;
+    public float fixedUpdateCounter;
     //Declare mr here
-    
+    private MeshRenderer mr;
 
     // Start is called before the first frame update
     void Start()
     {
         //Set mr to get MeshRenderer component here
-        
+        mr = GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
     void Update(){
         //disable MeshRenderer component below
-        
+        mr.enabled = false;
         //increment updateCounter by 1 below
-        
+        updateCounter += 1;
         //END OF CODE
         
         //Uncomment line below when update function is completed
-        //updateText.text = "Update Counter: " + updateCounter.ToString();
+        updateText.text = "Update Counter: " + updateCounter.ToString();
         
     }
     
@@ -36,7 +37,8 @@ public class BallController : MonoBehaviour
     void FixedUpdate()
     {
         //increment fixedUpdateCounter by 1 and update fixedUpdateText below
-
+        fixedUpdateCounter += 1;
+        fixedUpdateText.text = "fixedUpdateCounter:" + fixedUpdateCounter.ToString();
         
         //END OF CODE
     }
--- Assets/Scripts/InstantiateDestroy.cs
+++ Assets/Scripts/InstantiateDestroy.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class InstantiateDestroy : MonoBehaviour
 {
     //create variables below 
-    
+    public GameObject prefab;
+    public float delayDestroy = 0.5f;
 
     // Update is called once per frame
     void Update()
     {
         //WRITE CODE HERE
-        // if ()
-        // {
-            
-        // }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(prefab);
+        }
         //END OF CODE
+        Destroy(prefab,delayDestroy);
     }
 }
